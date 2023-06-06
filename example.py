0ef6259f6541b2d744784d22f659f64a6c29468b
Izotov <izotov93@yandex.ru>
2023-06-06 15:15:10 +0300

    ver 1.0.5 fix Update example, added function to read time series file by columns

--- example.py
+++ example.py
@@ -2,23 +2,57 @@ from NNetEn import NNetEn_entropy
 import numpy as np
 import re
 
-# Default Configuration
+'''
+    Default Configuration:
+        file : Path to the time series file    
+        ds : Dataset ('D1' - MNIST-10, 'D2' - SARS-CoV-2-RBV1)
+        mu : Usage fraction of the selected database
+        method: One of 6 methods for forming a reservoir matrix
+                   from the time series M1 ... M6.
+        metric: 'Acc' - accuracy metric,
+                'R2E' - R2 Efficiency metric,
+                'PE' - Pearson Efficiency metric.
+        epoch: The number of training epochs for the LogNNet neural network.
+        read_mode: Method to read file 
+                    ('Rows' reading by rows or 'Columns' reading by columns)
+'''
+
 config = {
     'file': 'Time_series_example.txt',
     'ds': 'D1',
-    'mu': 0.1,
+    'mu': 1,
     'method': 3,
     'metric': 'Acc',
-    'epoch':  5
+    'epoch':  5,
+    'read_mode' : 'Rows'
 }
 
-def read_file_time_series(input_file : str):
+def transpouse(mat):
+    matrix = []
+    for i in range(len(mat[0])):
+        matrix.append(list())
+        for j in range(len(mat)):
+            try:
+                matrix[i].append(mat[j][i])
+            except:
+                break
+    return matrix
+
+def read_file_time_series(input_file : str, mode='Rows'):
     result = []
     with open(input_file) as file:
-        for lines in file.readlines():
-            lines = re.sub(r'[ \t]+', " ", lines.strip(), 0)
-            result.append(np.array(lines.split(' ')).astype(float))
-    return result
+        line_list = file.readlines()
+        for lines in line_list:
+            lines = re.sub(r'[ \t]+', ' ',
+                           re.sub(r'[^(0-9.\- \t)]',
+                                  '', lines.strip()), 0)
+            if lines != '':
+                result.append(lines.split(' '))
+
+    if mode == 'Columns':
+        result = transpouse(result)
+
+    return [np.array(data).astype(float) for data in result]
 
 def main():
     # Print Configuration
@@ -26,33 +60,23 @@ def main():
     print('Dataset - {}, mu - {}'.format(
         config['ds'], str(config['mu'])))
     print('Method - {}, Metric - {}, Epoch - {}'.format
-          (str(config['method']), config['metric'], str(config['epoch'])))
+          (str(config['method']), config['metric'],
+           str(config['epoch'])))
+    print("Reading by {} from {}".format(config['read_mode'],
+                                                config['file']))
 
     # Reading the file containing time series
-    time_seties = read_file_time_series(config['file'])
-    print("Read {} time series from {}".format(len(time_seties),
-                                               config['file']))
+    time_seties = read_file_time_series(config['file'], config['read_mode'])
+    print("Read {} time series".format(len(time_seties)))
 
     # Creating a class with the selected database
-    # D1 - MNIST-10, D2 - SARS-CoV-2-RBV1
-    # mu - usage fraction of the selected database
     NNetEn = NNetEn_entropy(database=config['ds'], mu=config['mu'])
 
     #  Calculation the NNetEn value
-    # time_series: input data with a time series in numpy array format.
-    # epoch: The number of training epochs for the LogNNet neural network,
-    #           with a number greater than 0.
-    # method: One of 6 methods for forming a reservoir matrix
-    #           from the time series M1 ... M6.
-    # metric: 'Acc' - accuracy metric,
-    #         'R2E' - R2 Efficiency metric,
-    #          'PE' - Pearson Efficiency metric.
-    #  log: Parameter for logging the main data used in the calculation.
-    #       Recording is done in log.txt file.
-
     result = []
     for series in time_seties:
-        value = NNetEn.calculation(series, epoch=config['epoch'],
+        value = NNetEn.calculation(series,
+                                   epoch=config['epoch'],
                                    method=config['method'],
                                    metric=config['metric'],
                                    log=False)
